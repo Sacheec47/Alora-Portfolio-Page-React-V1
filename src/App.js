cf294f995b07b2e6c25a22215bcfeaf360a400fe
Sachithra <sachithra.kahatapitiya@gmail.com>
2018-03-02 01:09:35 +0530

    Nav bar has been added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import {red800} from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!!*** </h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
-export default App;
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//       <AppBar
+//         title="ALORA Web Design Studio"
+//         iconClassNameRight="muidocs-icon-navigation-expand-more"
+//       />
+//       </div>
+//     );
+//   }
+// // }
+
+
+const navBarStyle = getMuiTheme({
+  palette: {
+    backgroundColor: red800,
+  },
+  appBar: {
+    height: 40,
+  },
+});
+
+const Main = () => (
+   <MuiThemeProvider muiTheme={navBarStyle}>
+    <AppBar
+      title="ALORA Web Design Studio"
+      // iconClassNameRight="muidocs-icon-navigation-expand-more"
+      titleStyle={{ fontSize: '18px'}}
+      style={{ backgroundColor: red800}}
+    />
+  </MuiThemeProvider>
+);
+
+export default Main;
